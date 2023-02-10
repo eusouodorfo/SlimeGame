3374c689a629ff866e9c93a948b7d446775b3746
dorfollyud <rodolfo.dorfo@outlook.com>
2023-02-10 03:48:49 -0300

    inicio atualizacao layout

---
+++ Assets/Scripts/SettingsMenu.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SettingsMenu : MonoBehaviour
+{
+    public void SetVolume (float volume){
+        Debug.Log(volume); 
+    }
+}
