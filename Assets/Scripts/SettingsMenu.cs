20187f5177f5127a0ddc93f106c85dca949ea041
dorfollyud <rodolfo.dorfo@outlook.com>
2023-02-15 17:20:42 -0300

    settings em todas as fases

--- Assets/Scripts/SettingsMenu.cs
+++ Assets/Scripts/SettingsMenu.cs
@@ -6,11 +6,23 @@ using UnityEngine.Audio;
 public class SettingsMenu : MonoBehaviour{
 
     public AudioMixer audioMixer;
+    public AudioMixer audioMixerFx;
+    public AudioMixer audioMixerMusic;
 
     public void SetVolume (float volume){
         audioMixer.SetFloat("volume", volume);    
     }
 
-  
+    public void SetFxVolume (float fxVolume){
+        audioMixerFx.SetFloat("fxVolume", fxVolume);    
+    }
+
+    public void SetMusicVolume (float musicVolume){
+        audioMixerMusic.SetFloat("musicVolume", musicVolume);    
+    }
+
+    public void SetFullscreen (bool isFullscreen){
+        Screen.fullScreen = isFullscreen;
+    }
     
 }
