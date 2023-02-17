9a126b58b4a63d0d837b9d416d467da56b84a363
dorfollyud <rodolfo.dorfo@outlook.com>
2023-02-16 22:11:39 -0300

    trophy level 80%

--- Assets/Scripts/LevelSelectMenu.cs
+++ Assets/Scripts/LevelSelectMenu.cs
@@ -5,9 +5,21 @@ using UnityEngine.SceneManagement;
 
 public class LevelSelectMenu : MonoBehaviour
 {
-    public string mainMenu = "Main Menu";
+    public string mainMenu = "Main_Menu";
+    public string trophylevel = "trophylevel";
+    public string LevelSelect = "LevelSelect";
 
     public void GoToMainMenu(){
         SceneManager.LoadScene(mainMenu);
     }
+
+     public void GoToTrophyMenu(){
+        SceneManager.LoadScene(trophylevel);
+    }
+
+    public void GoToLevelSelectMenu(){
+        SceneManager.LoadScene(LevelSelect);
+    }
+
+
 }
