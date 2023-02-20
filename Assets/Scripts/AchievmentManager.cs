6ed3683e0916f5267058476ff9e4ae2061e1df39
dorfollyud <rodolfo.dorfo@outlook.com>
2023-02-20 16:36:28 -0300

    achievment manager iniciado e inicialmente funcionando

--- Assets/Scripts/AchievmentManager.cs
+++ Assets/Scripts/AchievmentManager.cs
@@ -1,18 +1,31 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class AchievmentManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+   public GameObject achievmentPrefab;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+   public Sprite[] sprites;
+
+   void Start(){
+        CreateAchievment("General", "Titulo Teste", "Descricao teste", 0);
+   }
+
+   public void CreateAchievment(string category, string title, string description, int  spriteIndex){
+    GameObject achievment = (GameObject) Instantiate(achievmentPrefab);
+
+    SetAchievmentInfo(category, achievment, title, description, spriteIndex);
+
+   }
+
+   public void SetAchievmentInfo(string category, GameObject achievment, string title, string description, int spriteIndex){
+        achievment.transform.SetParent(GameObject.Find(category).transform);
+        achievment.transform.localScale = new Vector3(1, 1, 1);
+        achievment.transform.GetChild(0).GetComponent<Text>().text = title;
+        achievment.transform.GetChild(1).GetComponent<Text>().text = description;
+        achievment.transform.GetChild(2).GetComponent<Image>().sprite = sprites[spriteIndex];
+
+   }
 }
