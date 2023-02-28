8bb455707b564614033ad3c8fe7d6153d58ed30c
dorfollyud <rodolfo.dorfo@outlook.com>
2023-02-28 17:34:32 -0300

    intro add ao jogo

---
+++ Assets/Scripts/Wait.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Wait : MonoBehaviour
+{
+   
+    public float wait_time = 5f;
+
+    void Start()
+    {
+        StartCoroutine(WaitForIntro());
+    }
+
+    IEnumerator WaitForIntro(){
+        yield return new WaitForSeconds(wait_time);
+        SceneManager.LoadScene(1);
+    }
+}
